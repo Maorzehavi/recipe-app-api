80d0f9f09083cc277a56899eab63da66c940b1c2
Maorz <maorzehavi@outlook.com>
2024-07-10 13:54:12 +0300

    adding postgres

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,3 +4,6 @@ from app import calc
 class CalcTests(SimpleTestCase):
     def test_add(self):
         self.assertEqual(calc.add(3,8),11)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(11,5),6)
