aad8989b8e504c1df97a01dcc21c66176a2e4480
Maorz <maorzehavi@outlook.com>
2024-07-10 14:00:39 +0300

    fixed lint

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,7 +3,7 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add(self):
-        self.assertEqual(calc.add(3,8),11)
+        self.assertEqual(calc.add(3,8), 11)
 
     def test_subtract(self):
-        self.assertEqual(calc.subtract(11,5),6)
+        self.assertEqual(calc.subtract(11,5), 6)
