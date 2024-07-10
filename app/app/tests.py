518216d7d554e58f7db3f472700cc5db7a375027
Maorz <maorzehavi@outlook.com>
2024-07-10 14:09:04 +0300

    fixed lint and postgres

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,11 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
+
     def test_add(self):
-        self.assertEqual(calc.add(3,8), 11)
+        self.assertEqual(calc.add(3, 8), 11)
 
     def test_subtract(self):
-        self.assertEqual(calc.subtract(11,5), 6)
+        self.assertEqual(calc.subtract(11, 5), 6)
