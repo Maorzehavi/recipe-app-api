2ff70c1707d99c527210629181286619c94a7c92
Maorz <maorzehavi@outlook.com>
2024-07-10 17:14:33 +0300

    starting wait for db test

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+'''
+django commanf to wait for the db to be available
+'''
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        pass
